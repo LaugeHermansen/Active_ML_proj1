cdd69df4a065483e2e4a5518c92f652eb386f455
Rebekka-M <73647490+Rebekka-M@users.noreply.github.com>
2022-02-24 14:51:28 +0100

    Updates

--- gp_optimize.py
+++ gp_optimize.py
@@ -2,12 +2,12 @@ import torch
 from botorch.models import SingleTaskGP
 from botorch.fit import fit_gpytorch_model
 from gpytorch.mlls import ExactMarginalLogLikelihood
-from botorch.optim import optimize_acqf
+from botorch.optim import optimize_acqf_mixed
 from botorch.acquisition import UpperConfidenceBound, ExpectedImprovement
 
 
 def get_next_hyperparameters(X, y, bounds = None):
-    # Introduce GP
+    # Introduce GP Surrogate Model
     gp = SingleTaskGP(X, y)
 
     # Fit hyperparameters of the GP
@@ -23,7 +23,7 @@ def get_next_hyperparameters(X, y, bounds = None):
     # TODO: Implement support for arbitrary acquisition function
     # acquisition_fn = UpperConfidenceBound(gp, beta=3)
     acquisition_fn = ExpectedImprovement(gp, best_f=max(y))
-    candidate, acq_value = optimize_acqf(
+    candidate, acq_value = optimize_acqf_mixed(
         acquisition_fn, bounds=bounds, q=1, num_restarts=1, raw_samples=1
     )
 
