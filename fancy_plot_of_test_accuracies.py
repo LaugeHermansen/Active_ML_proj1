7ddf55780c858c999e94430e996843b627e4bf1f
Lauge Hermansen <76589717+LaugeHermansen@users.noreply.github.com>
2022-02-24 15:51:04 +0100

    documented plot function

--- fancy_plot_of_test_accuracies.py
+++ fancy_plot_of_test_accuracies.py
@@ -3,7 +3,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_accuracies(accuracies: np.ndarray, legend, ylabel, xlabel, title, label_size = None, title_size = None):
+def plot_accuracies(accuracies: np.ndarray, legend, ylabel, xlabel, plot_title, label_size = None, title_size = None):
+        
+    """
+    function to plot accuracies and with current accuracies to compare optimization models
+
+    Parameters
+    ----------
+    accuracies :
+        numpy array of accuracies with shape (N,M) - N data points (iterations) and M optimization algorithms (probably just BO with GP vs random)
+    legend :    
+        the names of the optimizaion models
+    ylabel :    
+        you know
+    xlabel :    
+            -----
+    plot_title :
+            -----
+    label_size :
+        font size of x_label and y_label
+    title_size :
+        font size of title
+    """
+    
     N,M = accuracies.shape
     if len(legend) != M:  raise ValueError("The length of legend doesn't correspond with the number og lines to plot")
     
@@ -17,12 +39,12 @@ def plot_accuracies(accuracies: np.ndarray, legend, ylabel, xlabel, title, label
     plt.legend()
     plt.ylabel(ylabel, fontsize = label_size)
     plt.xlabel(xlabel, fontsize = label_size)
-    plt.title(title, fontsize = title_size)
+    plt.title(plot_title, fontsize = title_size)
     plt.show()
 
 
 if __name__ == "__main__":
-    N = 100
+    N = 200
     accuracy1 = np.log(np.arange(1,N+1))**3 + np.random.rand(N)*40
     accuracy2 = np.arange(N)*0.1 + np.random.rand(N)*40
     accuracies = np.vstack((accuracy1,accuracy2)).T
