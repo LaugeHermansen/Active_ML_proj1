Major refactor, fixed normalization bug, added bootstrapped start, larger bounds, lauge tqdm

gp_optimize.py
@@ -7,14 +7,21 @@ from botorch.optim import optimize_acqf_mixed
 from botorch.acquisition import UpperConfidenceBound, ExpectedImprovement
 from itertools import product
 
+def normalize(A, bounds):
+    return (A - bounds[0, :]) / (bounds[1, :] - bounds[0, :])
+
 
 def get_next_hyperparameters(X, y, bounds, feature_type):
     # Normalize X
-    X_norm = (X - bounds[0, :]) / (bounds[1, :] - bounds[0, :])
+    X_norm = normalize(X, bounds)
     
     # Normalize bounds
     bounds_norm = torch.tensor(np.ones_like(bounds))
     bounds_norm[0, :] = 0
+    
+    # Get normalized discrete feature list
+    feature_list_norm = generate_feature_list(bounds.detach().numpy(), feature_type)
+
 
     # Introduce GP
     gp = SingleTaskGP(X_norm, y)
@@ -23,19 +30,20 @@ def get_next_hyperparameters(X, y, bounds, feature_type):
     mll = ExactMarginalLogLikelihood(gp.likelihood, gp)
     fit_gpytorch_model(mll)
     
+
     # Bayesian Optimization
-    # acquisition_fn = UpperConfidenceBound(gp, beta=3)
     acquisition_fn = ExpectedImprovement(gp, best_f=max(y))
 
     candidate, acq_value = optimize_acqf_mixed(
         acquisition_fn,
-        fixed_features_list=generate_feature_list(bounds_norm, feature_type),
+        fixed_features_list=feature_list_norm,
         bounds=bounds_norm, q=1, num_restarts=1, raw_samples=1
     )
 
 
     # Unnormalize
     candidate = candidate * (bounds[1, :] - bounds[0, :]) + bounds[0, :]
+    candidate[:, feature_type] = torch.round(candidate[:, feature_type])
     
 
     # Return next hyperparameters to try, and the associated acquisition value
@@ -46,7 +54,9 @@ def generate_feature_list(bounds, feature_type):
     make_end_inclusive = np.array([0, 1])
 
     idx_discrete = np.argwhere(feature_type).squeeze()
-    values = {i: np.arange(*(bounds[:, i] + make_end_inclusive))
+    values = {i: normalize(np.arange(*(bounds[:, i] + make_end_inclusive), 
+                                     dtype=np.float), 
+                           bounds[:, i, None])
               for i in idx_discrete}
 
     discrete_feature_list = []
@@ -56,5 +66,6 @@ def generate_feature_list(bounds, feature_type):
                           for i, v in enumerate(values)}
 
         discrete_feature_list.append(feature_values)
+
 
     return discrete_feature_list

test.py
@@ -1,4 +1,5 @@
 #%%
+from re import I
 import botorch
 import torch
 from torchvision import datasets
@@ -10,16 +11,19 @@ from gp_optimize import get_next_hyperparameters
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
+from lauges_tqdm import tqdm
 
 
-def get_randoms(bounds, feature_type):
-    return [np.random.randint(bounds[0, i], bounds[1, i] + 1) if feature else np.random.uniform(*bounds[:, i]) for
-            i, feature in enumerate(feature_type)]
+# learning rate, weight decay, width, depth
+bounds = np.array([[0.0, 0.0, 1.0, 1.0],
+                   [8.0, 8.0, 4.0, 4.0]])
+feature_type = np.array([False, False, True, True])
+
+# Batch size, data loading workers
+batch_size, data_workers = 200, 12
 
 
-if __name__ == '__main__':
-    DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-    DEVICE = 'cpu'
+def load_data():
     transform = transforms.Compose([transforms.ToTensor()])
 
     training_set = datasets.EMNIST(root="./data", split="byclass", train=True,  download=True, transform=transform)
@@ -32,84 +36,160 @@ if __name__ == '__main__':
     validation_set = data_utils.Subset(test_set, val_indices)
     test_set = data_utils.Subset(test_set, test_indices)
 
-    train_dl = DataLoader(training_set, batch_size=200, num_workers=20)
-    validation_dl = DataLoader(validation_set, batch_size=200, num_workers=20)
-    test_dl = DataLoader(test_set, batch_size=200, num_workers=20)
+    train_dl = DataLoader(training_set, batch_size=batch_size, num_workers=data_workers)
+    validation_dl = DataLoader(validation_set, batch_size=batch_size, num_workers=data_workers)
+    test_dl = DataLoader(test_set, batch_size=batch_size, num_workers=data_workers)
 
-    #%%
-    accuracies_val = []
-    accuracies_test = []
-    accuracies_val_random = []
-    accuracies_test_random = []
-    #bounds = torch.tensor([[1e-6, 1e-6, 1, 1],
-    #                       [1/2, 1/2, 4, 4]])
-    bounds = torch.tensor([[1.0, 1.0, 1.0, 1.0],
-                           [4.0, 4.0, 4.0, 4.0]])
-    feature_type = [False, False, True, True] # lr, weight decay, width, depth
-    hypers = [np.array(get_randoms(bounds, feature_type))]
-    hypers_random = [np.array(get_randoms(bounds, feature_type))]
+    return train_dl, validation_dl, test_dl
 
-    acquisitions = []
 
-    current_best = 0
-    current_best_random = 0
+def get_random_hypers():
+    return [np.random.randint(bounds[0, i], bounds[1, i] + 1) if feature else np.random.uniform(*bounds[:, i]) for
+            i, feature in enumerate(feature_type)]
 
-    for i in range(1000):
-        # Instantiate and test model
-        net = model.CNN_class(width=hypers[-1][2], depth=hypers[-1][3])
-        net.cuda() if DEVICE == 'cuda' else net
-        model.train(net, train_dl, lr=10**(-hypers[-1][0]), weight_decay=10**(-hypers[-1][1]), n_epochs=2)
-        accuracy_val = model.test(net, validation_dl)
-        accuracy_test = model.test(net, test_dl)
 
-        if accuracy_val > current_best:
-            current_best = accuracy_val
-            torch.save(net.state_dict(), "best_model.pt")
+def get_random_hypers_acq(X, y, bounds, feature_type):
+    return get_random_hypers(), np.Infinity
 
-        # Track accuracy
-        accuracies_val.append(accuracy_val)
-        accuracies_test.append(accuracy_test)
 
-        np.save("hyperparameters", hypers)
-        np.save("accuracies_val", accuracies_val)
-        np.save("accuracies_test", accuracies_test)
+def get_bayesian_optimization_hypers_acq(X, y, bounds, feature_type):
+    # Get next hyper parameters
+    hypers, acqst = get_next_hyperparameters(
+        torch.tensor(X).reshape(-1, len(feature_type)),
+        torch.tensor(y).reshape(-1, 1),
+        bounds=torch.tensor(bounds),
+        feature_type=feature_type
+    )
 
-        # Get next hyper parameters
-        hyper_next, acqst = get_next_hyperparameters(
-            torch.tensor(hypers).reshape(-1, len(feature_type)),
-            torch.tensor(accuracies_val).reshape(-1, 1),
-            bounds=bounds,
-            feature_type=feature_type
+    # Return hypers and associated acquisition
+    return hypers.detach().numpy().squeeze(), acqst
+
+
+def train_test(log_learning_rate, log_weight_decay, width, depth):
+    net = model.CNN_class(width=width, depth=depth)
+    model.train(net, train_dl, lr=10**(-log_learning_rate), weight_decay=10**(-log_weight_decay), n_epochs=2)
+    accuracy_val = model.test(net, validation_dl)
+    accuracy_test = model.test(net, test_dl)
+    
+    return accuracy_val, accuracy_test, net
+
+
+def save_results(name, new_hypers, new_accuracy_val, new_accuracy_test, new_acquisition, model, hypers, accuracies_val, accuracies_test, acquisitions, start_iteration = 0):
+        # NOTE: Other than IO operations, this function is pure
+
+        # If the new accuracy is better than best known, update model
+        if len(accuracies_val[start_iteration:]) == 1 or new_accuracy_val > max(accuracies_val[start_iteration:]):
+            torch.save(model.state_dict(), f"results/{name}_best_model.pt")
+
+        # Append new hypers and accuracies to existing lists
+        hypers = hypers + [new_hypers]
+        accuracies_val = accuracies_val + [new_accuracy_val]
+        accuracies_test = accuracies_test + [new_accuracy_test]
+        acquisitions = acquisitions + [new_acquisition]
+
+        # Save accuracies and hyper parameters
+        np.save(f"results/{name}_hyperparameters", hypers)
+        np.save(f"results/{name}_accuracies_val", accuracies_val)
+        np.save(f"results/{name}_accuracies_test", accuracies_test)
+        np.save(f"results/{name}_acquisitions", acquisitions)
+        
+        # Return now arrays
+        return hypers, accuracies_val, accuracies_test, acquisitions
+
+
+def optimize_model(name, new_hypers_fn, iterations, bootstrap_model_name = None):
+    # Set random seeds
+    np.random.seed(9327642)
+    torch.manual_seed(830994)
+    torch.use_deterministic_algorithms(True)
+
+
+    # If bootstrapped points should be used, load them
+    if bootstrap_model_name:
+        hypers = list(np.load(f"results/{bootstrap_model_name}_hyperparameters.npy"))
+        accuracies_val = list(np.load(f"results/{bootstrap_model_name}_accuracies_val.npy"))
+        accuracies_test = list(np.load(f"results/{bootstrap_model_name}_accuracies_test.npy"))
+        acquisitions = list(np.load(f"results/{bootstrap_model_name}_acquisitions.npy"))
+
+        new_hypers, new_acquisition = new_hypers_fn(
+            hypers, accuracies_val,
+            bounds, feature_type
+        )
+    # Else, initialize with a random hyper parameter
+    else:
+        hypers = []
+        accuracies_val = []
+        accuracies_test = []
+        acquisitions = []
+
+        new_hypers, new_acquisition = get_random_hypers_acq(
+            hypers, accuracies_val, 
+            bounds, feature_type
         )
 
-        #Save next hyperparameters and associated acquisition
-        hypers.append(hyper_next.detach().numpy().squeeze())
-        acquisitions.append(acqst)
-        print(i, accuracies_val[-1])
 
-        # Random model:
-        net = model.CNN_class(width=hypers_random[-1][2], depth=hypers_random[-1][3])
-        net.cuda() if DEVICE == 'cuda' else net
-        model.train(net, train_dl, lr=10**(-hypers_random[-1][0]), weight_decay=10**(-hypers_random[-1][1]), n_epochs=2)
-        accuracy_val = model.test(net, validation_dl)
-        accuracy_test = model.test(net, test_dl)
+    # Track starting iteration
+    start_iteration = len(hypers) - 1
+    
+    
+    # Optimization loop
+    print(f"Optimizing via {name}...")
+    
+    for _ in tqdm(range(iterations), n_child_layers=3):
+        # WARN: Ensure order of parameters are same as function
+        new_accuracy_val, new_accuracy_test, model = train_test(*new_hypers)
 
-        if accuracy_val > current_best_random:
-            current_best_random = accuracy_val
-            torch.save(net.state_dict(), "best_model_random.pt")
+        # Save results
+        hypers, accuracies_val, accuracies_test, acquisitions \
+            = save_results(
+                name, 
+                new_hypers, new_accuracy_val, new_accuracy_test, new_acquisition, model, 
+                hypers, accuracies_val, accuracies_test, acquisitions, start_iteration
+            )
+            
+        # Get next hypers
+        new_hypers, new_acquisition = new_hypers_fn(
+            hypers, accuracies_val,
+            bounds, feature_type
+        )
+        
+        
+def create_bootstrap_samples(name = "bootstrap", iterations = 10):
+    # Set random seeds
+    np.random.seed(7231487)
+    torch.manual_seed(132082)
+    torch.use_deterministic_algorithms(True)
+    
+    # Generate and save bootstrap file
+    optimize_model(
+        name, 
+        get_random_hypers_acq,
+        iterations
+    )
 
-        # Track accuracy
-        accuracies_val_random.append(accuracy_val)
-        accuracies_test_random.append(accuracy_test)
 
-        np.save("hyperparameters_random", hypers_random)
-        np.save("accuracies_val_random", accuracies_val_random)
-        np.save("accuracies_test_random", accuracies_test_random)
-
-        hypers_random.append(np.array(get_randoms(bounds, feature_type)))
+# Load data globally
+train_dl, validation_dl, test_dl = load_data()
 
 
 
-#%%
-#%matplotlib widget
+if __name__ == '__main__':
 
+    # create_bootstrap_samples(
+    #     name="bootstrap", 
+    #     iterations=10
+    # )
+
+    optimize_model(
+        name="bayesian_optimization", 
+        new_hypers_fn=get_bayesian_optimization_hypers_acq,
+        iterations=400,
+        bootstrap_model_name="bootstrap"
+    )
+    
+    optimize_model(
+        name="random_optimizer", 
+        new_hypers_fn=get_random_hypers_acq,
+        iterations=4,
+        bootstrap_model_name="bootstrap"
+    )