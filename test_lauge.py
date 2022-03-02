ee8805acb348d0b8239025cb143b53e21aa95c64
Lauge Hermansen <76589717+LaugeHermansen@users.noreply.github.com>
2022-03-02 21:30:53 +0100

    'tqdm...'

--- test_lauge.py
+++ test_lauge.py
@@ -1,13 +1,13 @@
 from lauges_tqdm import tqdm
 
 import time
-for i in tqdm(range(5)):
-    for j in tqdm(range(13),1):
-        for j in tqdm(range(10),1):
+for i in tqdm(5):
+    for j in tqdm(13):
+        for j in tqdm(10):
             # for j in tqdm(range(3),1):
                 time.sleep(0.001)
-for i in tqdm(range(5)):
-    for j in tqdm(range(13),1):
+for i in tqdm(5,17):
+    for j in tqdm(13):
         # for j in tqdm(range(10),1):
             # for j in tqdm(range(3),1):
                 time.sleep(0.001)
